78fe41226e1d31fe8a483a9215fb14c712ae9350
Milan Mandaviya <milanmandaviya@Dhrumils-MacBook-Pro.local>
2025-01-29 17:45:18 +0530

    variable

---
+++ js/script.js
@@ -0,0 +1,17 @@
+let age = 24;
+const pi = 3.14;
+let name = "Milan";
+let valid = true;
+let value = null;
+
+console.log("My age is "+age);
+console.log(pi);
+console.log("My name is "+name);
+console.log("typeof valid "+typeof valid);
+console.log("typeof value "+typeof value);
+
+
+
+
+
+
