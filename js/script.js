ac5d9cc0de519bfbf4f48610f17c8f25f3ffbb5d
Milan Mandaviya <milanmandaviya@Dhrumils-MacBook-Pro.local>
2025-01-30 17:46:39 +0530

    operators

--- js/script.js
+++ js/script.js
@@ -10,8 +10,58 @@ console.log("My name is "+name);
 console.log("typeof valid "+typeof valid);
 console.log("typeof value "+typeof value);
 
+// swaping without third variable 
+
+let a=10;
+let b=20;
+
+ console.log("Before swaping a is:"+a);
+ console.log("Before swaping b is:"+b);
+
+ a=a+b;
+ b=a-b;
+ a=a-b;
+
+ console.log("After swaping a is:"+a);
+ console.log("After swaping b is:"+b);
+ 
+ 
+
+ 
+  // arithmetic operator
+
+  let j=10;
+  let k=5;
+
+  console.log(j+k);
+  console.log(j-k);
+  console.log("multiplication:"+j*k);
+  console.log("divison:"+j/k);
+  console.log("10 square is:"+j**2);
+  console.log("5 cube is:"+k**3);
+  console.log("10 square is:"+j**6);
+  console.log("post increment:"+j++);
+  console.log("pre increment:"+ ++j);
+
+  // grade calculate
+
+  let marks= parseInt
+  (prompt("enter marks"));
+  let avg=marks;
+  console.log("the average marks is:"+avg);
+
+  let grade=avg>=80?'A':avg>=60?'B':avg>50?'C':'pass';
+  console.log(grade);
+  
+            
+  
 
 
-
-
-
+  
+  
+  
+  
+  
+  
+  
+  
