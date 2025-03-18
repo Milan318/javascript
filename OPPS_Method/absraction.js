06df8ad2f00f2f20c5b70894388c60b5103a89fc
Milan318 <milanmandaviya7495@gmail.com>
2025-03-18 17:47:56 +0530

    done

--- OPPS_Method/absraction.js
+++ OPPS_Method/absraction.js
@@ -1,36 +1,74 @@
 
     // Data Abstrasction 
   
-   class Fruits{
-        printFruit(){
-            console.log("Eating fruits");
-        }
-   }
+//    class Fruits{
+//         printFruit(){
+//             console.log("Eating fruits");
+//         }
+//    }
 
-   class Apple extends Fruits{
-    printFruit(){
-        console.log("Eating apple");
+//    class Apple extends Fruits{
+//     printFruit(){
+//         console.log("Eating apple");
+//     }
+//    }
+
+//    class Pineapple extends Fruits{
+//     printFruit(){
+//         console.log("Eating pineapple");
+//     }
+//    }
+
+//    class Banana extends Fruits{
+//     printFruit(){
+//         console.log("Eating banana");   
+//     }
+//    }
+
+//    let FruitObj = new Fruits();
+//    FruitObj.printFruit();
+
+//    let FruitObj1 = new Pineapple();
+//    FruitObj1.printFruit();
+
+//    let FruitObj2 = new Banana();
+//    FruitObj2.printFruit();
+
+
+class Employee {
+    constructor(name, age, salary) {
+        this.E_name = name;
+        this.age = age;
+        this.salary = salary;
     }
-   }
 
-   class Pineapple extends Fruits{
-    printFruit(){
-        console.log("Eating pineapple");
+    setEmp(name, age, salary) {
+        this.E_name = name;
+        this.age = age;
+        this.salary = salary;
     }
-   }
 
-   class Banana extends Fruits{
-    printFruit(){
-        console.log("Eating banana");   
+    getEmp() {
+        console.log("Employee Name: " + this.E_name + " Age: " + this.age + " Salary: " + this.salary);
     }
-   }
+}
 
-   let FruitObj = new Fruits();
-   FruitObj.printFruit();
+class Salary extends Employee {
+    constructor(name, age, salary, hra, ma, pf) {
+        super(name, age, salary);
+        this.hra = hra;
+        this.ma = ma;
+        this.pf = pf;
+        this.grossSalary = this.hra + this.ma - this.pf;
+        this.netSalary = this.grossSalary + this.salary;
+    }
 
-   let FruitObj1 = new Pineapple();
-   FruitObj1.printFruit();
+    getSal() {
+        super.getEmp();
+        console.log(`Employee grossSalary is ${this.grossSalary} and netSalary is ${this.netSalary}`);
+    }
+}
 
-   let FruitObj2 = new Banana();
-   FruitObj2.printFruit();
 
+let sal = new Salary("Bheem", 19, 60000, 3500, 2000, 4500);
+sal.getSal();
