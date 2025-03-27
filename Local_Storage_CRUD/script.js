1e4f68fdcca50bc36cff8b899cd8173d1107ba8e
Milan318 <milanmandaviya7495@gmail.com>
2025-03-27 17:33:12 +0530

    done

--- Local_Storage_CRUD/script.js
+++ Local_Storage_CRUD/script.js
@@ -3,6 +3,9 @@ let password = document.getElementById('password');
 let email = document.getElementById('email');
 let form = document.getElementById('form');
 let showData = document.querySelector('#table tbody');
+let update = document.getElementById('update');
+let edit = -1;
+
 
 let users=[];
 username.focus();
@@ -33,10 +36,32 @@ const display = () =>{
             <td>${user.username}</td>
             <td>${user.password}</td>
             <td>${user.email}</td>
+            <td>
+                <button class="btn btn-danger" onclick="deleteData(${index})">Delete</button>
+                <button class="btn btn-warning" onclick="editData(${index})">Edit</button>
+            </td>
         `
           showData.append(row);
 
     });
+}
+
+let deleteData=(index)=>{
+    users.splice(index,1);
+    display();
+}
+
+let editData=(index)=>{
+    let user = users.filter((_,idx)=>idx==index)[0];
+
+    usernme.value=user.username;
+    password.value=user.password;
+    email.value=user.email;
+
+    update.innerHTML="Update";
+    update.classList.remove('btn-primary');
+    update.classList.add('btn-success');
+
 }
 
 display();
